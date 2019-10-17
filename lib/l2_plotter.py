097b56fd9039c1d51933b9afe6eab121a2cbf62a
Alex Georges <georges.alexander@bcg.com>
2019-10-17 09:30:29 -0700

    made plotter easier to work with

--- lib/l2_plotter.py
+++ lib/l2_plotter.py
@@ -28,38 +28,46 @@ def file_opener(file):
     return cutoffs
 
 
-def l2_plotter():
-    results_dir = '../results_final/fashion/60k'
-    stamp = '60k Fashion'
-    noise_model = 'S&P'
-    thresholds = np.linspace(1, 4.5, 100)
+def l2_plotter(data_set, size, lower_bound, upper_bound, noise_model):
+
+
+    noise_model = noise_model.replace(" ", "_")
+
+    results_dir = '../results_final/%s/%s' %(data_set,size)
+    stamp = '%s %s' %(size, str.capitalize(data_set))
+
+    thresholds = np.linspace(lower_bound, upper_bound , 100)
 
     fig, ax = plt.subplots()
 
-    ######## MNIST, 10k
-    # architectures = ['PCA', 'CAE','DAE','VAE', 'CNN']
-    # c = ['blue', 'deepskyblue','cyan','slateblue', 'red']
-    # initial_accuracies = [9453, 9361, 9399, 9495, 9700]
 
-    ######## MNIST 60k
-    # architectures = ['PCA', 'VAE', 'CNN']
-    # c = ['blue', 'slateblue', 'red']
-    # initial_accuracies = [9733,9768, 9851]
-
-    ######## FASHION, 10k
-    # architectures=['PCA','CAE','VAE','CNN']
-    # c = ['blue', 'deepskyblue', 'slateblue','red']
-    # initial_accuracies=[8164,8127,8050,8935]
-
-    ######## FASHION 60k
-    architectures = ['PCA', 'VAE', 'CNN']
-    c = ['blue', 'slateblue', 'red']
-    initial_accuracies = [8672, 8757, 9336]
+    if data_set=='mnist':
+        if size=='10k':
+            ######## MNIST, 10k
+            architectures = ['PCA', 'CAE','DAE','VAE', 'CNN']
+            c = ['blue', 'deepskyblue','cyan','slateblue', 'red']
+            initial_accuracies = [9453, 9361, 9399, 9495, 9700]
+        elif size=='60k':
+            ######## MNIST 60k
+            architectures = ['PCA', 'VAE', 'CNN']
+            c = ['blue', 'slateblue', 'red']
+            initial_accuracies = [9733,9768, 9851]
+    elif data_set=='fashion':
+        if size=='10k':
+            ######## FASHION, 10k
+            architectures=['PCA','CAE','VAE','CNN']
+            c = ['blue', 'deepskyblue', 'slateblue','red']
+            initial_accuracies=[8164,8127,8050,8935]
+        elif size=='60k':
+            ######## FASHION 60k
+            architectures = ['PCA', 'VAE', 'CNN']
+            c = ['blue', 'slateblue', 'red']
+            initial_accuracies = [8672, 8757, 9336]
 
     accuracies = {}
 
     for n, architecture in enumerate(architectures):
-        file = '%s/l2norms_mlp_s&p_%s.csv' % (results_dir, architecture)
+        file = '%s/l2norms_mlp_%s_%s.csv' % (results_dir, str.lower(noise_model),architecture)
         accuracies[(architecture, noise_model)] = []
         l2norms = file_opener(file)
         print(min(l2norms), max(l2norms))
@@ -71,12 +79,15 @@ def l2_plotter():
             accuracies[(architecture, noise_model)].append(ratio)
         plt.plot(thresholds, accuracies[(architecture, noise_model)], label='%s' % (architecture), color=c[n])
 
-    plt.legend(loc="lower left")
-    plt.xlabel('$l^2$-Norm')
-    plt.ylabel('Normalized Accuracy')
+    plt.legend(loc="lower left", fontsize=12)
+    plt.xlabel('$l^2$-Norm', fontsize=12)
+    plt.ylabel('Normalized Accuracy', fontsize=12)
     ax.grid()
     # ax.set_yscale('log')
-    plt.title("Normalized Classification Accuracy, %s, %s" % (stamp, noise_model))
+    noise_model = noise_model.replace("_", " ")
+    plt.title("Normalized Classification Accuracy, %s, %s" % (stamp, noise_model), fontsize=12)
+    noise_model = noise_model.replace(" ", "_")
+    stamp = stamp.replace(" ", "_")
     plt.savefig('../results_final/Total_L2_Accuracy_%s_%s.pdf' % (stamp, noise_model), bbox_inches='tight',
                 pad_inches=0)
     plt.show()
--- src/doit.py
+++ src/doit.py
@@ -15,9 +15,30 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 pymapper('trueexamples_in.csv', 'trueexamples_in.csv', '../data_train_fashion_unnormalized',
          '../data_test_fashion_unnormalized', projection='pca', filter_type=None, train='yes', map_method='cluster4',
          dimensions=1, component=1, num_int=10, num_bin=10, idx='yes')
 pymapper('trueexamples_in.csv', 'trueexamples_in.csv', '../data_train_fashion_unnormalized',
          '../data_test_fashion_unnormalized', projection='pca', filter_type=None, train='no', map_method='cluster4',
          dimensions=1, component=1, num_int=10, num_bin=10, idx='yes')
+
+
+
+l2_plotter('mnist', '10k', 1, 5, 'Gauss Blur')
+l2_plotter('mnist', '10k', 2, 5, 'Gaussian')
+l2_plotter('mnist', '10k', 1, 5, 'S&P')
+
+l2_plotter('mnist', '60k', 1, 5, 'Gauss Blur')
+l2_plotter('mnist', '60k', 2, 5, 'Gaussian')
+l2_plotter('mnist', '60k', 1, 5, 'S&P')
+
+
+l2_plotter('fashion', '10k', 0, 4, 'Gauss Blur')
+l2_plotter('fashion', '10k', 2, 5, 'Gaussian')
+l2_plotter('fashion', '10k', 1, 4.5, 'S&P')
+
+l2_plotter('fashion', '60k', 0, 4, 'Gauss Blur')
+l2_plotter('fashion', '60k', 2, 5, 'Gaussian')
+l2_plotter('fashion', '60k', 1, 4.5, 'S&P')
+
